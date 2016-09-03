76b4131239521e34c98acd4cb4af251452251367
luhtonen <luhtonen@gmail.com>
2016-09-03 14:58:45 +0200

    Replacing the Default Template Engine

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"database/sql"
 	"encoding/json"
 	"encoding/xml"
-	"html/template"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 
 	"github.com/codegangsta/negroni"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/yosssi/ace"
 )
 
 // Page struct
@@ -38,20 +38,23 @@ func verifyDatabase(w http.ResponseWriter, r *http.Request, next http.HandlerFun
 }
 
 func main() {
-	templates := template.Must(template.ParseFiles("templates/index.html"))
-
 	db, _ = sql.Open("sqlite3", "dev.db")
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template, err := ace.Load("templates/index", "", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
 		p := Page{Name: "Gopher"}
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
 		p.DBStatus = db.Ping() == nil
 
-		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+		if err = template.Execute(w, p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
